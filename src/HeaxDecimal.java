d9c8157f82488333b5aa328bead2731279afc29b
vanTHkrab <vanth2548@gmail.com>
2024-06-25 09:37:18 +0700

    .

--- src/HeaxDecimal.java
+++ src/HeaxDecimal.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-
 public class HeaxDecimal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter interger: ");
         int num = sc.nextInt();
-
-        if (num < 10 ) {
-            System.out.println("HexaDecimal = " + num);
-        } else if (num >= 10 && num <= 15) {
-            System.out.println("HexaDecimal = " + (char)(num % 10 + 65));
-        } else {
-            System.out.println("Worning number");
-        }
-
-        // String hex = Integer.toHexString(num);
-        // System.out.println("Hexadecimal: " + hex);
+        String hex = Integer.toHexString(num);
+        System.out.println("Hexadecimal: " + hex);
     }
 }
+
+
+
+
+// if (num < 10 ) {
+//     System.out.println("HexaDecimal = " + num);
+// } else if (num >= 10 && num <= 15) {
+//     System.out.println("HexaDecimal = " + (char)(num % 10 + 65));
+// } else {
+//     System.out.println("Worning number");
+// }
+
 
